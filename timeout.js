3d5355be233b07b4cc8515e785d7f624cfb360c1
guanshiyu <876222431@qq.com>
2018-05-07 15:38:24 +0800

    add timeout.js

---
+++ timeout.js
@@ -0,0 +1,8 @@
+console.log("Begin...");
+new Promise(function(resolve,reject){
+	setTimeout(function(){
+		resolve("guanshiyu");
+	},3000);
+}).then(function(value){
+	console.log("Hello "+value);
+});
